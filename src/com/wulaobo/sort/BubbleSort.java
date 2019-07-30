92f72eb85d306448b489ef5d0aee265f85ef1550
wulaobo <wulaobo@aliyun.com>
2019-07-30 16:54:51 +0800

    写了冒泡排序和选择排序

---
+++ src/com/wulaobo/sort/BubbleSort.java
@@ -0,0 +1,55 @@
+package com.wulaobo.sort;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+
+//冒泡排序
+public class BubbleSort {
+
+    public static void main(String[] args) {
+//        int[] arr =new int[80000];
+          int[] arr = {5,4,3,2,1};
+//        for(int i = 0;i<80000;i++) {
+//            arr[i] = (int) (Math.random()*8000000);
+//        }
+
+//        Date date1 = new Date();
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//        String date1Str =sdf.format(date1);
+//        System.out.println("排序前的时间是:"+date1Str);
+
+
+        int length = arr.length;
+        int temp = 0;
+        boolean flag = false;
+        for(int i = 0;i<length-1;i++) {
+            for (int j = 0;j<length-1-i;j++) {
+                if(arr[j]>arr[j+1]) {
+                    flag = true;
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+
+            System.out.println("第"+(i+1)+"趟排序的结果为："+ Arrays.toString(arr));
+
+            if(!flag) {
+                break;
+            }else{
+                flag = false;
+            }
+
+
+        }
+
+
+//        Date date2 = new Date();
+//       // SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//        String date2Str =sdf.format(date2);
+//        System.out.println("排序后的时间是:"+date2Str);
+
+    }
+
+}
---
+++ src/com/wulaobo/sort/SelectSort.java
@@ -0,0 +1,50 @@
+package com.wulaobo.sort;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+
+//选择排序
+public class SelectSort {
+
+    public static void main(String[] args) {
+//        int[] arr = {5,4,3,2,1};
+        int[] arr = new int[80000];
+        for (int i = 0;i<arr.length;i++) {
+             arr[i] = (int) (Math.random()*8000000);
+        }
+
+        Date date1 = new Date();
+        String date1Str = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(date1);
+        System.out.println("排序前的时间为："+date1Str);
+
+        selectSort(arr);
+        Date date2 = new Date();
+        String date2Str = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(date2);
+        System.out.println("排序后的时间为："+date2Str);
+//        System.out.println("排序后的结果为:"+ Arrays.toString(arr));
+    }
+
+    public static void selectSort(int[] arr) {
+        int length = arr.length;
+        //外层循环表示循环的趟数
+        for(int i = 0;i<length-1;i++) {
+            //默认第一个元素为最小值
+            int min = arr[i];
+            int minIndex = i;
+            for(int j = i+1;j<length;j++) {
+                if(arr[j]<min) {
+                    arr[i] = arr[j];
+                    minIndex = j;
+                }
+            }
+            if(minIndex!=i){
+                arr[minIndex] = min;
+            }
+
+
+        }
+    }
+
+
+}
